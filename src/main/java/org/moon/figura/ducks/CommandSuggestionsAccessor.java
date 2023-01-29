37b2010a9ac64cee8964faf1983f80835b59aaec
applejuiceyy <30875598+applejuice-wastaken@users.noreply.github.com>
2023-01-29 00:46:42 +0000

    fix stuff + full implementation

--- src/main/java/org/moon/figura/ducks/CommandSuggestionsAccessor.java
+++ src/main/java/org/moon/figura/ducks/CommandSuggestionsAccessor.java
@@ -1,5 +1,12 @@
 package org.moon.figura.ducks;
 
+import com.mojang.brigadier.suggestion.Suggestions;
+
 public interface CommandSuggestionsAccessor {
     void setUseFiguraSuggester(boolean use);
+
+    interface SuggestionBehaviour {}
+    record AcceptBehaviour(Suggestions suggest) implements SuggestionBehaviour {}
+    record HintBehaviour(String hint, int pos) implements SuggestionBehaviour {}
+    record RejectBehaviour(String err) implements SuggestionBehaviour {}
 }
